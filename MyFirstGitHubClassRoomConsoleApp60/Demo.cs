7267b1c73c792e36023d961847f4a9f8436f9964
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-08-27 11:03:06 +0200

    Added ToString in Demo class

--- MyFirstGitHubClassRoomConsoleApp60/Demo.cs
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -11,5 +11,10 @@ namespace MyFirstGitHubClassRoomConsoleApp60
         public int id { get; set; }
         public string name { get; set; }
         public string description { get; set; }
+
+        public override string ToString() 
+        {
+            return $"{id}, {name} \n Beskrivelse: {description}";
+        }
     }
 }
